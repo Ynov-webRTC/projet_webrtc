062ceb1572b277df185fa79e9a3536d0d5966b6d
nacimgoura <BTEIQi9BhoaBF7OAp>
2016-10-30 16:02:10 +0100

    Généré une URL de visionnage du stream, modif interface

---
+++ app_webrtc/controller/stream.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var uniqid = require('uniqid');
+
+var Stream = function () {
+    console.log("stream");
+};
+
+Stream.prototype.create = function (req) {
+    return req.get('host') + "/" + uniqid();
+};
+
+Stream.prototype.start = function (id) {
+    console.log("lancement du stream " + id);
+
+};
+
+module.exports = Stream;
--- app_webrtc/routes/index.js
+++ app_webrtc/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var Stream = require('../controller/stream');
+var colors = require('colors/safe');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {title: 'WebRTC'});
+});
+
+router.get('/create', function (req, res, next) {
+    var stream = new Stream();
+    var url = stream.create(req);
+    res.render('create', {url: url});
+});
+
+router.post('/start', function (req, res, next) {
+    var id_stream = req.body.id_stream;
+    var stream = new Stream();
+    stream.start(id_stream);
+    res.render('create', {title: 'WebRTC'});
 });
 
 module.exports = router;
